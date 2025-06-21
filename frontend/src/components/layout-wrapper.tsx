182e3879a07c18725b168468f16632b12eadd5f9
ParsaJafarian <parsajafarian72@gmail.com>
2025-06-21 14:05:05 -0700

    Fix layout issue

--- frontend/src/components/layout-wrapper.tsx
+++ frontend/src/components/layout-wrapper.tsx
@@ -3,22 +3,18 @@ import { Card, CardContent } from "@/components/ui/card"
 import Header from "./header"
 
 interface LayoutWrapperProps {
-  children: React.ReactNode
+    children: React.ReactNode
 }
 
 export default function LayoutWrapper({ children }: LayoutWrapperProps) {
-  return (
-    <div className="min-h-screen bg-orange-50 text-gray-100">
-      <Header />
-      <div className="container mx-auto p-4">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 h-[calc(100vh-120px)]">
-          <div className="lg:col-span-2 flex flex-col gap-4">
-            <Card className="flex-1 bg-white border-orange-200">
-              <CardContent className="h-full p-6">{children}</CardContent>
-            </Card>
-          </div>
+    return (
+        <div className="min-h-screen bg-orange-50 text-gray-100">
+            <Header />
+            <div className="container mx-auto p-4">
+                <Card className="bg-white border-orange-200">
+                    <CardContent className="p-6">{children}</CardContent>
+                </Card>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
