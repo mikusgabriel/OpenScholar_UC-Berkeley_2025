61e00d1054ed01d3b45095185e4dc74ff214b0a8
ParsaJafarian <parsajafarian72@gmail.com>
2025-06-21 15:14:03 -0700

    Remove padding

--- frontend/src/components/layout-wrapper.tsx
+++ frontend/src/components/layout-wrapper.tsx
@@ -1,5 +1,4 @@
 import type React from "react"
-import { Card, CardContent } from "@/components/ui/card"
 import Header from "./header"
 
 interface LayoutWrapperProps {
@@ -8,13 +7,11 @@ interface LayoutWrapperProps {
 
 export default function LayoutWrapper({ children }: LayoutWrapperProps) {
     return (
-        <div className="min-h-screen bg-orange-50 text-gray-100">
+        <>
             <Header />
-            <div className="container mx-auto p-4">
-                <Card className="bg-white border-orange-200">
-                    <CardContent className="p-6">{children}</CardContent>
-                </Card>
-            </div>
-        </div>
+            <main className="flex-1 overflow-hidden">
+                <div className="h-full p-4">{children}</div>
+            </main>
+        </>
     )
 }
