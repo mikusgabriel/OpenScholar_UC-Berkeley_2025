857e84b2315c3be6ff8a0a6fe5541464c3ff5488
Gabriel Mikus <113815444+mikusgabriel@users.noreply.github.com>
2025-06-21 11:49:59 -0700

    open ai chat functions squeleton

--- backend/versionner.py
+++ backend/versionner.py
@@ -1,7 +1,33 @@
+from openai import OpenAI
 from uagents import Agent
 from schema import Request
+import dotenv
+
+dotenv.load_dotenv()
+
 
 versionner = Agent("versionner",seed="versionner",port=8001,endpoint="http://localhost:8001/submit")
+
+CHAT_MODEL = "gpt-4.1-nano"
+PROMPT_TEMPLATE = ""
+tools = ""
+# will have to implement github functions with open ai chat functions?
+
+def query_openai_chat(query: str) -> str:
+    client = OpenAI(
+        api_key=dotenv.get_key("OPENAI_API_KEY"), 
+    )
+
+    chat_completion = client.responses.create(
+        model=CHAT_MODEL,
+        input=[
+            {"role": "system", "content": PROMPT_TEMPLATE},
+            {"role": "user", "content": query}
+        ],
+        tools=tools,
+    )
+    return chat_completion.output
+
 
 @versionner.on_message(model=Request)
 async def handle_review(ctx, sender: str, msg):
