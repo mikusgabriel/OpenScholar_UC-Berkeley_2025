6feff9c29fc5250cfdaf3aaca0a0027eeed8aea8
emilrl <razer.saiso@gmail.com>
2025-06-21 12:38:24 -0700

    supabse

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+import asyncio
+from exporter import exporter as agent1
+from versionner import versionner as agent2
+from orchestrator import orchestrator as orchestrator
+from orchestrator import agent as orchestrator
+
+async def main():
+    await asyncio.gather(
+        agent1.run_async(),
+        agent2.run_async(),
+        orchestrator.run_async()
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
